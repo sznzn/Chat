0970baafd34b8c1da792912f42c56cff798e6012
Philippe <philippe.kurz@gmail.com>
2022-07-12 17:05:27 +0200

    mise en ligne

--- header.php
+++ header.php
@@ -1,7 +1,11 @@
 <?php
-$dsn = 'mysql:dbname=chatlt;host=localhost';
-$user = 'root';
-$password = '';
+// $dsn = 'mysql:dbname=chatlt;host=localhost';
+// $user = 'root';
+// $password = '';
+$dsn = 'mysql:dbname=dbs8004373;host=db5009437228.hosting-data.io';
+$user = 'dbu2731491';
+$password = 'f7dM5CE6r';
+
 try {
     $bdd = new PDO($dsn, $user, $password, array(PDO::ATTR_ERRMODE => PDO:: ERRMODE_EXCEPTION));
 } catch (PDOException $e) {
